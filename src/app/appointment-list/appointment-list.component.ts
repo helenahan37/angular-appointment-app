3956cded25b26129eeb05e204c149bc278d91904
Helena.Han <hlyhelena37@gmail.com>
2024-02-03 23:46:10 +1100

    feat: create add new appointment method

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Appointment } from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrl: './appointment-list.component.css',
+})
+export class AppointmentListComponent {
+  newAppointmentTitle: string = '';
+  newAppointmentDate: Date = new Date();
+
+  appointments: Appointment[] = [];
+
+  //add new appointment
+  addAppointment() {
+    if (this.newAppointmentDate && this.newAppointmentTitle.trim().length) {
+      let newAppointment: Appointment = {
+        id: Date.now(),
+        title: this.newAppointmentTitle,
+        date: this.newAppointmentDate,
+      };
+      this.appointments.push(newAppointment);
+
+      //clear the input
+      this.newAppointmentTitle = '';
+      this.newAppointmentDate = new Date();
+
+      alert(this.appointments.length);
+    }
+  }
+
+  //delete a new appointment
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
+  }
+}
