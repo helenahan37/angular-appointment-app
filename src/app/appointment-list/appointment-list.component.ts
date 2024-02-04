1762d8b0c99d1331e1129f3d4d1ded3d14ffd1e9
Helena.Han <hlyhelena37@gmail.com>
2024-02-04 22:26:56 +1100

    load saved appointments

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css',
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent implements OnInit {
   newAppointmentTitle: string = '';
   newAppointmentDate: Date = new Date();
 
   appointments: Appointment[] = [];
+
+  //load saved appointments
+  ngOnInit(): void {
+    let savedAppointments = localStorage.getItem('appointments');
+    //check if there are saved appointments and parse JSON string to array
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments) : [];
+  }
 
   //add new appointment
   addAppointment() {
@@ -25,11 +33,14 @@ export class AppointmentListComponent {
       //clear the input
       this.newAppointmentTitle = '';
       this.newAppointmentDate = new Date();
+
+      localStorage.setItem('appointments', JSON.stringify(this.appointments));
     }
   }
 
   //delete a new appointment
   deleteAppointment(index: number) {
     this.appointments.splice(index, 1);
+    localStorage.setItem('appointments', JSON.stringify(this.appointments));
   }
 }
